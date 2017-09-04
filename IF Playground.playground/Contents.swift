8ff6d8e615b87c352de7336e0f4ca3f00f5cbac4
jinho kim <Bigwin10103@gmail.com>
2017-09-04 19:52:00 +0900

    IF 예제) 무조건 브라켓으로 감싸줘야함

---
+++ IF Playground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a:Int = 1
+let b = 1
+
+if(a<4 && b<4)
+{
+    print("branch 1")
+}
+else if(a == 10)
+{
+    print("branch 2")
+}
+else{
+    print("branch 3")
+}
+
+
