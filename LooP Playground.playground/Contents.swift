a19b903a8b72c08196945436011e84c610c4ec34
jinho kim <Bigwin10103@gmail.com>
2017-09-04 20:21:31 +0900

    no message

---
+++ LooP Playground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var index:Int = 1
+
+for index in 1...10{
+    print("*")
+    for count in 1...5{
+        print("%")
+    }
+}
+
+var sum = 0
+
+for index in 1...5
+{
+    sum += index
+    print(sum)
+}
+
+var num:Int = 10
+
+While true{
+    
+    print("true")
+    num=num-1
+}
---
+++ Switch Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var someCharacter:Character = "d"
+
+switch someCharacter {
+case "c":
+    print("c")
+default:
+    print("not c")
+}
+
